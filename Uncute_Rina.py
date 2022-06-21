6522eb286de37d4aff2669ad3c531bc024fa40fa
FlorisFireball <65396931+FlorisFireball@users.noreply.github.com>
2022-06-21 21:26:40 +0200

    update version for cool-ness

--- Uncute_Rina.py
+++ Uncute_Rina.py
@@ -31,7 +31,7 @@ RinaDB = cluster["Rina"]
 #       manage roles (for adding/removing table roles)
 
 # dumb code for cool version updates
-fileVersion = "1.0.2.3".split(".")
+fileVersion = "1.0.2.4".split(".")
 try:
     version = open("version.txt", "r").read().split(".")
 except:
