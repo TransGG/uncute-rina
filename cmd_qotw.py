ffbb81043ff92551c5b52d9e408def76bd4cb616
MysticMia <65396931+MysticMia@users.noreply.github.com>
2023-08-22 21:20:58 +0200

    Add more logging to find issue: not sending logs to thread
    still trying to figure out how to send logs to a thread when badeline gives a user a punishment. i've currently set up a way to see badeline's log message after someone is punished. I've probably also figured out the target user, the reason, and private notes from the embed, but it doesn't yet send the message to the watch-out-doubts thread of the target person, while i'm certain they should be in there. Perhaps the first message in a thread doesn't include the starter message. Or perhaps you can get a starter message from thread.get_message(thread.id) (if the message.id == thread.id (according to api page))

--- cmd_qotw.py
+++ cmd_qotw.py
@@ -332,6 +332,15 @@ class QOTW(commands.Cog):
         watch_channel = self.client.get_channel(self.client.custom_ids["staff_watch_channel"])
         for thread in watch_channel.threads:
             async for starter_message in thread.history(limit=1, oldest_first=True):
+                
+                debug("\nthread: "+thread.name+"\nstarter message: "+starter_message.content+"\nembeds: "+bool(starter_message.embeds))
+                for embed in starter_message.embeds:
+                    dict = embed.to_dict()
+                    if "author" in dict:
+                        debug("embed author: "+dict["author"])
+                    else:
+                        debug("embed: "+dict)
+
                 if len(starter_message.embeds) == 0:
                     continue
                 if starter_message.embeds[0].author.url.split("/")[3] == reported_user_id:
