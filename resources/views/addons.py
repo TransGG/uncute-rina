1008ad1af7c46d9d50502dd1b46191e2a5033872
MysticMia <xmysticmia@gmail.com>
2024-07-07 23:07:35 +0200

    fix faulty find-and-replace causing AttributeError for discord.ui.ButtonStyle

--- resources/views/addons.py
+++ resources/views/addons.py
@@ -5,7 +5,7 @@ from resources.customs.bot import Bot
 class EqualDex_AdditionalInfo(discord.ui.View):
     def __init__(self, url):
         super().__init__()
-        link_button = discord.ui.Button(style = discord.ui.ButtonStyle.gray,
+        link_button = discord.ui.Button(style = discord.ButtonStyle.gray,
                               label = "More info",
                               url = url)
         self.add_item(link_button)
@@ -17,7 +17,7 @@ class SendPublicButton_Math(discord.ui.View):
         self.client = client
         self.timeout = timeout
 
-    @discord.ui.button(label='Send Publicly', style=discord.ui.ButtonStyle.gray)
+    @discord.ui.button(label='Send Publicly', style=discord.ButtonStyle.gray)
     async def send_publicly(self, itx: discord.Interaction, _button: discord.ui.Button):
         self.value = 1
         await itx.response.edit_message(content="Sent successfully!")
