14e215ef7d5064046ea18063ba8503bfb7b0bb18
Madeline "Maddie" Osiris <maddie@saxen.tech>
2024-10-25 20:36:54 -0500

    Update cmd_ban_appeal_reactions.py to log reactions

--- extensions/cmd_ban_appeal_reactions.py
+++ extensions/cmd_ban_appeal_reactions.py
@@ -7,14 +7,32 @@ from resources.customs.bot import Bot
 class BanAppealReactionsAddon(commands.Cog):
     def __init__(self, client: Bot):
         self.client = client
-
+        
+    """
+    ToDo
+    - Make Rina have buttons instead of reactions
+    - Make the buttons do something
+    - Make the buttons unban if a number of upvotes cast are greater than the downvotes
+    - Make the Veto button deny all other requests
+    - Make the buttons count upwards when votes are cast or show a text indicator... somehow
+    """
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author.id in self.client.custom_ids["ban_appeal_webhook_ids"]:
             await message.add_reaction("👍")
             await message.add_reaction("🤷")
             await message.add_reaction("👎")
+            await message.add_reaction("❌")
 
 
 async def setup(client):
     await client.add_cog(BanAppealReactionsAddon(client))
+
+## ToDo - Remove later.
+@bot.listen()
+async def on_reaction_add(reaction, user):
+    print("The following reactions have been added:")
+    print(on_reaction_add.reaction)
+    print(on_reaction_add.user)
+# do something with reaction and user
+
