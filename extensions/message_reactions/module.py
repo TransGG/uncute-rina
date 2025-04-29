f32cd7c2054fa27aa65058b371f8ae23aa0d3a3b
MysticMia <xmysticmia@gmail.com>
2025-04-29 17:47:11 +0200

    Move hardcoded webhook id to ServerAttributes, and add module to EnabledModules to allow toggleability.
    Also fixes ban_appeal_webhook not being parsable (webhooks must be fetched, so I opted for only storing the ID), and ints not parsing in server_settings.py

--- extensions/message_reactions/module.py
+++ extensions/message_reactions/module.py
@@ -1,9 +1,13 @@
 from resources.customs.bot import Bot
 
-from extensions.message_reactions.cogs import AEGISPingReactionsAddon
-from extensions.message_reactions.cogs import BanAppealReactionsAddon
+from extensions.message_reactions.cogs import (
+    AEGISPingReactionsAddon,
+    AnonReportsReactionsAddon,
+    BanAppealReactionsAddon
+)
 
 
 async def setup(client: Bot):
     await client.add_cog(AEGISPingReactionsAddon(client))
+    await client.add_cog(AnonReportsReactionsAddon(client))
     await client.add_cog(BanAppealReactionsAddon(client))
