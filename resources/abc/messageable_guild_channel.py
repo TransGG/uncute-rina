a0037de37dee0f6d6a02d24b547c6ce43da54847
MysticMia <65396931+MysticMia@users.noreply.github.com>
2026-05-23 17:14:38 +0200

    Move get_or_fetch_channel to MessageableGuildChannel file
    starboard was the only one using this function and it's practically only
    used for guild channels; which is also what starboard already expected
    to receive so yeah might as well.

--- resources/abc/messageable_guild_channel.py
+++ resources/abc/messageable_guild_channel.py
@@ -1,7 +1,5 @@
 import discord
-
-
-__all__ = ['MessageableGuildChannel']
+import typing
 
 
 type MessageableGuildChannel = (
@@ -10,3 +8,35 @@ type MessageableGuildChannel = (
     | discord.StageChannel
     | discord.Thread
 )
+
+
+async def get_or_fetch_messageable_guild_channel(
+        client: discord.Client,
+        channel_id: int
+) -> (
+        MessageableGuildChannel | None
+):
+    """
+    Use client.get_channel or client.fetch_channel if None
+
+    :param client: The client with which to fetch the channel.
+    :param channel_id: The channel to get or fetch.
+    :raise discord.InvalidData: An unknown channel type was received
+     from Discord.
+    :raise discord.Forbidden: No permission to fetch this channel.
+    :raise discord.HTTPException: Retrieving the channel failed.
+    """
+    ch = client.get_channel(channel_id)
+    if ch is not None:
+        assert isinstance(ch, MessageableGuildChannel.__value__)
+        ch = typing.cast(MessageableGuildChannel, ch)
+        return ch
+
+    try:
+        ch = await client.fetch_channel(channel_id)
+        assert isinstance(ch, MessageableGuildChannel.__value__)
+        ch = typing.cast(MessageableGuildChannel, ch)
+    except discord.NotFound:
+        return None
+
+    return ch
--- resources/utils/discord_utils.py
+++ resources/utils/discord_utils.py
@@ -11,37 +11,6 @@ if TYPE_CHECKING:
     from resources.customs import Bot
 
 
-async def get_or_fetch_channel(
-        client: discord.Client,
-        channel_id: int
-) -> (
-        discord.abc.GuildChannel
-        | discord.Thread
-        | discord.abc.PrivateChannel
-        | None
-):
-    """
-    Use client.get_channel or client.fetch_channel if None
-
-    :param client: The client with which to fetch the channel.
-    :param channel_id: The channel to get or fetch.
-    :raise discord.InvalidData: An unknown channel type was received
-     from Discord.
-    :raise discord.Forbidden: No permission to fetch this channel.
-    :raise discord.HTTPException: Retrieving the channel failed.
-    """
-    ch = client.get_channel(channel_id)
-    if ch:
-        return ch
-
-    try:
-        ch = await client.fetch_channel(channel_id)
-    except discord.NotFound:
-        return None
-
-    return ch
-
-
 def send_channel_or_interaction(
         itx: discord.Interaction
 ) -> typing.Callable[
