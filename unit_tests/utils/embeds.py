5803c9bd36607db2b09ebc87fefcbb0d1f0572d5
MysticMia <65396931+MysticMia@users.noreply.github.com>
2026-06-12 17:51:34 +0200

    refactor(tests/embeds): reduce get_embed_issues complexity
    From 23 to (3, 7) by using a match-case statement and making a helper
    function, _validate_embed_field, giving a max complexity of 7.

--- unit_tests/utils/embeds.py
+++ unit_tests/utils/embeds.py
@@ -1,6 +1,8 @@
 import discord
 from typing import TYPE_CHECKING
 
+from discord.types.embed import EmbedField
+
 if TYPE_CHECKING:
     from discord.types import embed as d_embed
 
@@ -24,74 +26,81 @@ def get_embed_issues(embed: discord.Embed) -> tuple[list[str], int]:
 
     embed_dict = embed.to_dict()
     for key, value in embed_dict.items():
-        if key == "title":
-            total_characters += len(value)
-            if len(value) > 256:
-                issues.append(
-                    f"Title length exceeds 256 characters (is '{len(value)}')."
-                )
+        match key:
+            case "title":
+                total_characters += len(value)
+                if len(value) > 256:
+                    issues.append(
+                        f"Title length exceeds 256 characters (is '{len(value)}')."
+                    )
 
-        elif key == "description":
-            total_characters += len(value)
-            if len(value) > 4096:
-                issues.append(
-                    f"Description length exceeds 4096 characters "
-                    f"(is '{len(value)}')."
-                )
+            case "description":
+                total_characters += len(value)
+                if len(value) > 4096:
+                    issues.append(
+                        f"Description length exceeds 4096 characters "
+                        f"(is '{len(value)}')."
+                    )
 
-        elif key == "fields":
-            value: list[d_embed.EmbedField]
-            if len(value) > 25:
-                issues.append(
-                    f"Embed contains more than 25 fields (is '{len(value)}')."
-                )
-            for field in value:
-                for field_key, field_value in field.items():
-                    if field_key == "type":
-                        if field_value != "rich":
+            case "fields":
+                value: list[d_embed.EmbedField]
+                if len(value) > 25:
+                    issues.append(
+                        f"Embed contains more than 25 fields (is '{len(value)}')."
+                    )
+                for field in value:
+                    for field_key, field_value in field.items():
+                        char_count, issue_text = _validate_embed_field(field, field_key, field_value)
+                        total_characters += char_count
+                        if issue_text is not None:
+                            issues.append(issue_text)
+
+            case "footer":
+                value: d_embed.EmbedFooter
+                text = value["text"]
+                total_characters += len(text)
+                if len(text) > 2048:
+                    issues.append(
+                        f"Footer length exceeds 2048 characters "
+                        f"(is '{len(text)}')."
+                    )
+
+            case "author":
+                value: d_embed.EmbedAuthor
+                for author_key, author_value in value.items():
+                    if author_key == "name":
+                        total_characters += len(author_value)
+                        if len(author_value) > 256:
                             issues.append(
-                                f"Field type '{field_value}' (for field with "
-                                f"name '{field['name']}') is not supported."
+                                f"Author name '{author_value}' exceeds 256 "
+                                f"characters (is '{len(author_value)}')."
                             )
-                    if field_key == "name":
-                        total_characters += len(field_value)
-                        if len(field_value) > 256:
-                            issues.append(
-                                f"Field name '{field_value}' exceeds 256 "
-                                f"characters (is '{len(field_value)}')."
-                            )
-                    if field_key == "value":
-                        total_characters += len(field_value)
-                        if len(field_value) > 1024:
-                            issues.append(
-                                f"Field value for field with name "
-                                f"'{field['name']}' exceeds 1024 characters "
-                                f"(is '{len(field_value)}')."
-                            )
-
-        elif key == "footer":
-            value: d_embed.EmbedFooter
-            text = value["text"]
-            total_characters += len(text)
-            if len(text) > 2048:
-                issues.append(
-                    f"Footer length exceeds 2048 characters "
-                    f"(is '{len(text)}')."
-                )
-
-        elif key == "author":
-            value: d_embed.EmbedAuthor
-            for author_key, author_value in value.items():
-                if author_key == "name":
-                    total_characters += len(author_value)
-                    if len(author_value) > 256:
-                        issues.append(
-                            f"Author name '{author_value}' exceeds 256 "
-                            f"characters (is '{len(author_value)}')."
-                        )
 
     if total_characters > 6000:
         issues.append(f"Total character count of embed exceeds 6000 "
                       f"characters (is '{total_characters}').")
 
     return issues, total_characters
+
+
+def _validate_embed_field(field: EmbedField, field_key: str, field_value: str) -> tuple[int, str | None]:
+    if field_key == "type":
+        if field_value != "rich":
+            return 0, (
+                f"Field type '{field_value}' (for field with "
+                f"name '{field['name']}') is not supported."
+            )
+    if field_key == "name":
+        if len(field_value) > 256:
+            return len(field_value), (
+                f"Field name '{field_value}' exceeds 256 "
+                f"characters (is '{len(field_value)}')."
+            )
+    if field_key == "value":
+        if len(field_value) > 1024:
+            return len(field_value), (
+                f"Field value for field with name "
+                f"'{field['name']}' exceeds 1024 characters "
+                f"(is '{len(field_value)}')."
+            )
+    return 0, None
